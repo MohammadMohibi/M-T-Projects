20d78d67b8f725bf97abfbd667b43c8149c5aa82
Mohammad Mohibi <mohammad.mohibi@ventia.com>
2024-09-26 12:58:13 +1000

    Adding regex functionality

--- M-T-Projects/Invoice Reconciliation/main.py
+++ M-T-Projects/Invoice Reconciliation/main.py
@@ -9,15 +9,23 @@ def main():
     folLoc = input("Provide location of Folders: ")
     folLoc = str(checkIfBlank(folLoc,"location"))
     
-    
+    # Continue through all pdf files in folder to output the sum of each type of fee
     for file in os.listdir(folLoc):
         filename = os.fsdecode(file)
+        
+        # Check for pdf
         if filename.endswith(".pdf"):
             print(file)
+            pdfText = extract_text(f'{folLoc}\\{filename}')
+
+            # series of lists for the position and sum of each type of fee per invoice
+            esFee = sum([float(pdfText[x.end():x.end()+4]) for x in re.finditer("Establishment Fee", pdfText)])
+            print(esFee)
+            '''
             for page in extract_pages(f'{folLoc}\\{filename}'):
                 for element in page:
                     print(element)
-
+            '''
 
         else:
             print(f'{filename} is not a pdf')
