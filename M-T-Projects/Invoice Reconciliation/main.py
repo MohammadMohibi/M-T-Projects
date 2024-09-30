f40db832f02ea57808f5b3a87502e8dd4cb5ed24
Mohammad Mohibi <mohammad.mohibi@ventia.com>
2024-09-30 16:14:03 +1000

    Fixing line number posting

--- M-T-Projects/Invoice Reconciliation/main.py
+++ M-T-Projects/Invoice Reconciliation/main.py
@@ -3,6 +3,9 @@ from pdfminer.high_level import extract_pages, extract_text
 import pypdf, openpyxl, os, re
 from helpers import *
 
+def findIteration(pdf, searchTerm):
+
+    return sum([float(pdf[x.end():x.end()+4]) for x in re.finditer(searchTerm, pdf)])
 
 def main():
     # User provides the location of the folders where invoices are
@@ -19,8 +22,25 @@ def main():
             pdfText = extract_text(f'{folLoc}\\{filename}')
 
             # series of lists for the position and sum of each type of fee per invoice
-            esFee = sum([float(pdfText[x.end():x.end()+4]) for x in re.finditer("Establishment Fee", pdfText)])
-            print(esFee)
+
+            # Finds the position and adds the volume of that fee in the tuple (line#, sum)
+            mealFee = (58, findIteration(pdfText,r"Meal Allowance"))
+            esFee = (60, findIteration(pdfText,r"Establishment Fee"))
+            twoxnightFee = (12, findIteration(pdfText,r"2 x Traffic Controller Night Rate 0-8 hrs"))
+            twoxnightplusFee = (24, findIteration(pdfText,r"2 x Traffic Controller Night Rate 8\+ hrs"))
+            tmanightFee = (15, findIteration(pdfText,r"TMA with Operator Night Rate 0-8 hrs"))
+            tmanightplusFee = (27, findIteration(pdfText,r"TMA with Operator Night Rate 8\+ hrs"))
+            podFee = (52, findIteration(pdfText,r"Drop Deck / Pod Truck"))
+            onexnightFee = (11, findIteration(pdfText,r"1 x Traffic Controller Night Rate 0-8 hrs"))
+            onexnightplusFee = (23, findIteration(pdfText,r"1 x Traffic Controller Night Rate 8\+ hrs"))
+            portFee = (55, findIteration(pdfText,r"Portaboom"))
+            vmsFee = (48, findIteration(pdfText,r"VMS Ute with Operator Night Rate 0-8 hrs"))
+            vmsplusFee = (49, findIteration(pdfText,r"VMS Ute with Operator Night Rate 8\+ hrs"))
+
+            # Sort them into order of the line number so it is easier to copy
+            invoiceList = (filename, sorted([mealFee,esFee,twoxnightFee, twoxnightplusFee,tmanightFee, tmanightplusFee, podFee,onexnightFee,onexnightplusFee,portFee,vmsFee,vmsplusFee]))
+            print(invoiceList)
+
             '''
             for page in extract_pages(f'{folLoc}\\{filename}'):
                 for element in page:
@@ -32,3 +52,4 @@ def main():
 
 
 main()
+
